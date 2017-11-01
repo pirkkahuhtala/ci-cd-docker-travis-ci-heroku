47b5fae02dcba2a53e59bdeb98dc87a4035aae6f
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-01 18:58:18 +0200

    Create Hello world and add Dockerfile

---
+++ index.js
@@ -0,0 +1,12 @@
+'use stric'
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3333, function() {
+  console.log('Example app listening on port 3000!');
+});
