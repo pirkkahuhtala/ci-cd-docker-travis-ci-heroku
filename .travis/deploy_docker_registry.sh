e58c70ab2ac074a2c0a2e3a86a2af35fe8c4644d
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-01 21:45:34 +0200

    Fix

--- .travis/deploy_docker_registry.sh
+++ .travis/deploy_docker_registry.sh
@@ -8,11 +8,13 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 
-if [ "$DOCKER_REGISTRY_URL" = "docker.io"]; then
+if [ "$DOCKER_REGISTRY_URL" = "docker.io" ]; then
   DOCKER_IMAGE="$DOCKER_USER/$HEROKU_APP_NAME:$TAG"
 else
   DOCKER_IMAGE="$DOCKER_REGISTRY_URL/$DOCKER_USER/$HEROKU_APP_NAME:$TAG" 
 fi
+
+echo $DOCKER_IMAGE;
 
 docker build -f Dockerfile -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
