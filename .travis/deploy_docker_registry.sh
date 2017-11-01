dfdd9b819dc02e5563a0969e1ad6109329917966
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-01 20:54:32 +0200

    Fix Docker registry deploy script

--- .travis/deploy_docker_registry.sh
+++ .travis/deploy_docker_registry.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY_URL
+
+if [ "$DOCKER_REGISTRY_URL" = "docker-io"]; then
+  REAL_DOCKER_REGISTRY_URL=""
+else
+  REAL_DOCKER_REGISTRY_URL="$DOCKER_REGISTRY_URL/"
+fi
+
+docker login -u $DOCKER_USER -p $DOCKER_PASS $REAL_DOCKER_REGISTRY_URL
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t "$DOCKER_REGISTRY_URL/$HEROKU_APP_NAME:$TAG" .
+docker build -f Dockerfile -t "$REAL_DOCKER_REGISTRY_URL$HEROKU_APP_NAME:$TAG" .
 docker push "$DOCKER_REGISTRY_URL/$HEROKU_APP_NAME:$TAG"
