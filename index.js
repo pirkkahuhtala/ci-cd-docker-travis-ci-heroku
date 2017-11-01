33c505c6db33d4f8481bc4a7c4d442bc309bdf1f
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-01 21:25:04 +0200

    Hello Maailma

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Maailma!');
 });
 
 app.listen(port, function() {
