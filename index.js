6ed618b33410ec0059b657b5c4a3710c4998bbba
Pirkka Huhtala <pirkka.huhtala@weave.fi>
2017-11-01 21:02:28 +0200

    Fix port

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3333, function() {
+app.listen(port, function() {
   console.log('Example app listening on port 3000!');
 });
